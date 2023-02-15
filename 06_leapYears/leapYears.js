0a6440c4298092c21bc83055a5b450ff776d3315
pgodettejr <paul.godette@gmail.com>
2023-02-15 12:27:19 -0500

    Completed TDD on these exercises
    Each exercise works as intended. Fixed all errors.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,47 @@
-const leapYears = function() {
+// ChatGPT solution
 
-};
+const leapYears = function(year) {
+    return (year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0);
+  };
+  
+// My solution (incorrect)
+
+/* const leapYears = function() {
+  let year = 0
+
+  for (let i = 0; i <= 99999; i++)
+    if (i % 4 !== 0) {
+      return 'ERROR';
+    } else if (i % 100 !== 0 && i % 400 !== 0) {
+      return 'ERROR';
+    } else {
+      year += i;
+    }
+
+    return year;
+}; */
+
+// ChatGPT literally writing a better version of my code (that still isn't as good as it's initial answer)
+
+/* const leapYears = function() {
+    let count = 0;
+    for (let i = 0; i <= 9999; i++) {
+      if (isLeapYear(i)) {
+        count++;
+      }
+    }
+    return count;
+  };
+  
+  const isLeapYear = function(year) {
+    return (year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0);
+  }; */
+
+// TOP solution (better than ChatGPT solution)
+
+/* const leapYears = function(year) {
+  return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0);
+}; */
 
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,22 @@
-const convertToCelsius = function() {
+// ChatGPT solution
+
+const convertToCelsius = (fahrenheit) => {
+  return Math.round(((fahrenheit - 32) * 5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = (celsius) => {
+  return Math.round((celsius * 9/5 + 32) * 10) / 10;
 };
+
+// TOP solution (almost exactly like ChatGPT's, slightly better formatting)
+
+/* const convertToCelsius = function(fahrenheit) {
+  return Math.round((fahrenheit - 32) * (5/9) * 10) / 10;
+};
+
+const convertToFahrenheit = function(celsius) {
+  return Math.round(((celsius * 9/5) + 32) * 10) / 10;
+}; */
 
 // Do not edit below this line
 module.exports = {
